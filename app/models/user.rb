e94fc77f98a4d4f4627f7180ea3f2bf3a3918342
johnnydangfit <johnny.dang.goldenowl@gmail.com>
2023-01-05 23:00:47 +0700

    Update login ui and signup ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,6 @@ class User < ApplicationRecord
   rolify
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   def auth_token
     JsonWebToken.encode(user_id: id, user_email: email)
   end
